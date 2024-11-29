6f69bee1aac6a38bbdefe92d328e294cdcc3fabb
Julia <julianunesle1@gmail.com>
2024-11-29 02:51:22 -0300

    Add SvgLoader and SCSS

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  css:{
-    preprocessorOptions:{
-      scss: {additionalData : `@import "src/style.scss";`}
+  plugins: [vue(), svgLoader()],
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@import "src/style.scss";` }
     }
   }
 })
