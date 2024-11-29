7d2860c5327022bf6807fcb98dc05e709fae5b7a
Julia <julianunesle1@gmail.com>
2024-11-29 02:52:06 -0300

    Config pages and routes

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,25 +1,40 @@
 <template>
-    
-    <div class="">
-       
-        <div>Cafezinho</div>
 
-    </div>
+  <div class="products-container">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
 </template>
 <script>
-
-export default{
-    data() {
+import Card from '../components/ProductCard.vue'
+export default {
+  components: {
+    Card
+  },
+  data() {
     return {
       name: 'Kelly'
     }
   },
   methods: {},
   mounted() {
-  //  console.log('Abri!')
+    //  console.log('Abri!')
   }
 }
 </script>
 <style lang="scss">
-
+.products-container {
+  margin-block: 20px;
+  margin-inline: auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 16px;
+  justify-content: center;
+  padding: 16px;
+  @include container-max-width()
+}
 </style>
