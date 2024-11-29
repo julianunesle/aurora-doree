e27ab7702d630d410f311291d05862ff8ee6cb2a
Julia <julianunesle1@gmail.com>
2024-11-29 02:52:44 -0300

    Cart card item and menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,34 +1,106 @@
 <template>
-  <nav>
-
-  <menuIcon/>
-    <ul>
+  <nav class="principal-menu">
+    sasa
+    <ul class="menu-list">
       <RouterLink to="/" exact>Home</RouterLink>
       <RouterLink to="/login">Login</RouterLink>
       <RouterLink to="/cadastrar">Cadastro</RouterLink>
     </ul>
+    <span>
+
+      <button @click="goToCart">
+        <shoppingCart class="svg-icon menu" />
+      </button>
+      <button @click="toggleMenu">
+        <menuCloseIcon class="svg-icon menu" v-if="showMenu" />
+        <menuIcon class="svg-icon menu" v-else />
+      </button>
+    </span>
   </nav>
 </template>
 
 <script>
-import { RouterLink, RouterView } from 'vue-router';
-import menuIcon from '../assets/icons/menu-open.svg'
-import SVGIcon from './SVGIcon.vue';
-// import iconMenu from '../assets/icons/menu-open.svg'
+import { RouterLink } from 'vue-router';
+// import menuIcon from '../assets/vue.svg'
+import menuIcon from '../assets/icons/menu.svg'
+import menuCloseIcon from '../assets/icons/close.svg'
+import shoppingCart from '../assets/icons/shopping-cart.svg'
 export default {
   name: 'Menu',
-  components:{
-    SVGIcon
+  components: {
+    menuIcon, menuCloseIcon,shoppingCart
   },
   data() {
     return {
-      icone : menuIcon
+      showMenu: false
+
+    }
+  },
+  methods: {
+    toggleMenu() {
+      const menuList = document.getElementsByClassName('menu-list')[0]
+      this.showMenu = !this.showMenu
+      this.showMenu ? menuList.classList.add('show-menu') : menuList.classList.remove('show-menu')
+    },
+    goToCart(){
+      this.$router.push('/cart')
     }
   }
 }
 </script>
-<style scoped>
-.read-the-docs {
-  color: #888;
+<style lang="scss" scoped>
+.principal-menu {
+  background: $background;
+  height: 8dvh;
+  overflow-y: visible;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  padding-inline: 36px;
+  color: $white;
+  button {
+    background: none;
+    cursor: pointer;
+
+    .svg-icon {
+      width: 45px;
+      height: 47px;
+
+      path {
+        stroke: antiquewhite !important;
+        color: antiquewhite;
+        fill: wheat;
+      }
+    }
+  }
+
+  ul {
+    display: none;
+
+    &.show-menu {
+      display: flex;
+      flex-direction: column;
+      width: 50%;
+      background: $background;
+      height: max-content;
+      position: absolute;
+      top: 8dvh;
+      right: 0;
+    }
+
+
+    a {
+      display: inline-flex;
+      width: 100%;
+      padding: 12px;
+      color: $white;
+
+      &:visited {
+        color: rgba($white ,.1);
+      }
+    }
+
+  }
+
 }
 </style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,64 @@
+<template>
+    <RouterLink to="/produto/1">
+    <div class="card">
+        <div class="img-wrap">
+
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/d/d8/Blue_Bottle%2C_Kyoto_Style_Ice_Coffee_%285909775445%29.jpg">
+        </div>
+        <span class="info">
+            <h2>
+                Titulo
+            </h2>
+            <!-- <p>Ännu en berömd och utsökt kall dryck för dem som föredrar choklad. Tänk dig smaken av en shake med
+                choklad och vispad grädde på toppen.</p> -->
+            <span> R$2.89 <button>+</button></span>
+        </span>
+    </div>
+</RouterLink>
+</template>
+<script>
+import { RouterLink } from 'vue-router';
+</script>
+<style lang="scss">
+.card {
+    margin: 10px;
+    width: 250px;
+    height: 350px;
+    background: white;
+    border-radius: 8px;
+    overflow: hidden;
+    cursor: pointer;
+    transition: .5s;
+
+    &:hover {
+        box-shadow: 8px 8px 16px $palebrown;
+    }
+
+    .img-wrap {
+        width: 100%;
+        height: 250px;
+        overflow: hidden;
+
+        img {
+            width: 100%;
+            height: 250px;
+        }
+    }
+
+    .info {
+        padding-inline: 8px;
+        display: inline-block;
+
+        h2 {
+            text-align: center;
+        }
+
+        p {
+            text-align: justify;
+        }
+    }
+
+
+}
+</style>
