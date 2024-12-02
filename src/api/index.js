d945669f6dad0e2af8d4ca26a7138c57d18f6cd3
julianunesle <83466605+julianunesle@users.noreply.github.com>
2024-12-02 18:47:37 +0000

    add endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,5 @@
+let devEnv = window.location.hostname === 'localhost'
+
 export const api_url = 'https://api.sampleapis.com/coffee'
 
-export const api_aurora_doree = 'http://localhost:3535/'
+export const api_aurora_doree = devEnv ? 'http://localhost:3535/' : 'https://aurora-doree-be-production.up.railway.app/'
